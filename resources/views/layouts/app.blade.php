6b0cf5826e60ea59cdf552c628733d36eb43b951
rabucejo <rogerhapay@gmail.com>
2023-04-15 15:38:36 +0800

    imports

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @vite(['resources/sass/app.scss', 'resources/sass/_variable.scss', 'resources/js/app.js', 'resources/js/scripts.js', 'resources/js/datatables-simple-demo.js', 'resources/assets/demo/chart-area-demo.js', 'resources/assets/demo/chart-bar-demo.js', 'resources/assets/demo/chart-pie-demo.js', 'resources/assets/demo/datatables-demo.js', 'resources/assets/demo/img/error-404-monochrome.svg', 'resources/css/app.css', 'resources/css/styles.css'])
 </head>
 
 <body>
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,23 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import path from "path";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
+                "resources/sass/app.scss",
+                "resources/sass/_variable.scss",
+                "resources/js/app.js",
+                "resources/js/scripts.js",
+                "resources/js/datatables-simple-demo.js",
+                "resources/assets/demo/chart-area-demo.js",
+                "resources/assets/demo/chart-bar-demo.js",
+                "resources/assets/demo/chart-pie-demo.js",
+                "resources/assets/demo/datatables-demo.js",
+                "resources/assets/demo/img/error-404-monochrome.svg",
+                "resources/css/app.css",
+                "resources/css/styles.css",
             ],
             refresh: true,
         }),
